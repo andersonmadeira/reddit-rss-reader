20b3be09ee34ae3d947171537e64cd45b3a08c75
Anderson De Oliveira Madeira <anderson.madeira@geq.com.br>
2017-08-25 17:16:14 -0300

    Added filters

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, ActionSheetController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -9,12 +9,14 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-  public feeds: Array<string>;
+  public feeds: Array<any>;
+  public noFilter: Array<any>;
+  public hasFilter: boolean = false;
   private url: string = "https://www.reddit.com/new.json";
   private olderPosts: string = "https://www.reddit.com/new.json?after=";
   private newerPosts: string = "https://www.reddit.com/new.json?before=";
 
-  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController, public actionSheetCtrl: ActionSheetController) {
     this.fetchContent();
   }
 
@@ -27,8 +29,19 @@ export class HomePage {
 
     this.http.get(this.url).map(res => res.json()).subscribe(data => {
       this.feeds = data.data.children;
+
+      this.feeds.forEach((e, i, a) => {
+        if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+          e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+        }
+      })
+
+      this.noFilter = this.feeds;
+
       loading.dismiss();
     });
+
+
   }
 
   public doInfinite(infiniteScroll) {
@@ -43,6 +56,8 @@ export class HomePage {
         }
       });
 
+      this.noFilter = this.feeds;
+      this.hasFilter = false;
       infiniteScroll.complete();
     });
   }
@@ -57,8 +72,57 @@ export class HomePage {
           e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
         }
       });
+
+      this.noFilter = this.feeds;
+      this.hasFilter = false;
       refresher.complete();
     });
+  }
+
+  public showFilters() :void {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Filter options:',
+      buttons: [
+        {
+          text: 'Music',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() == "music");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Games',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "gaming");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Pictures',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "pics");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Ask Reddit',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "askreddit");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.feeds = this.noFilter;
+            this.hasFilter = false;
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
   }
 
 }

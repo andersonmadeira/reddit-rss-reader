7fbb1b49f8fd11c96b8467ff84fdb2c797e34ced
Anderson De Oliveira Madeira <anderson.madeira@geq.com.br>
2017-08-25 15:36:20 -0300

    Added a Loading progress dialog

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -12,10 +12,20 @@ export class HomePage {
   public feeds: Array<string>;
   private url: string = "https://www.reddit.com/new.json";
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+    this.fetchContent();
+  }
+
+  private fetchContent(): void {
+    let loading = this.loadingCtrl.create({
+      content: 'Fetching content...'
+    });
+
+    loading.present();
+
     this.http.get(this.url).map(res => res.json()).subscribe(data => {
       this.feeds = data.data.children;
-      console.log(this.feeds);
+      loading.dismiss();
     });
   }
 

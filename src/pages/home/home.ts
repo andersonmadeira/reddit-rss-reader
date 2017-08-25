2849822dacdbce17e4ac8552b23cf90355734081
Anderson De Oliveira Madeira <anderson.madeira@geq.com.br>
2017-08-25 16:40:45 -0300

    Infinite Scroll added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { InAppBrowser } from 'ionic-native';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -11,6 +12,7 @@ export class HomePage {
 
   public feeds: Array<string>;
   private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
 
   constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
     this.fetchContent();
@@ -26,6 +28,22 @@ export class HomePage {
     this.http.get(this.url).map(res => res.json()).subscribe(data => {
       this.feeds = data.data.children;
       loading.dismiss();
+    });
+  }
+
+  public doInfinite(infiniteScroll) {
+    let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1].data.name : "";
+
+    this.http.get(this.olderPosts + paramsUrl).map(res => res.json()).subscribe(data => {
+      this.feeds = this.feeds.concat(data.data.children);
+
+      this.feeds.forEach((e, i, a) => {
+        if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
+          e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+        }
+      });
+
+      infiniteScroll.complete();
     });
   }
 
